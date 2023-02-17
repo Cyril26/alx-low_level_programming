600bf45a10daa9c2bfa200f542aa17ffa1b694fc
Cyril26 <cyrilodm26@gmail.com>
2023-02-17 13:27:47 -0800

    add 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghhijklmnopqrstuvwxyz";
+
+	for(int i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar("\n");
+	return (0);
+}
