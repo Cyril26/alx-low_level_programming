3d5728cc45702303be03ca52a6e62c9438a2d672
Cyril26 <cyrilodm26@gmail.com>
2023-02-17 13:51:34 -0800

    add 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+ *
+ * Return - Always (0) success
+ */
+
+int main(void)
+{
+	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alpha[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
