281f4f085b17e6153a48581d98f6e7a3f13a24c6
Cyril26 <cyrilodm26@gmail.com>
2023-02-21 13:10:30 -0800

    add 2-print_alphabet_x10.c file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-
-	for (i = 97; i <= 122; i++)
+	int i, j = 0;
+	while(j < 10)
 	{
-		_putchar(i);
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+	
+		_putchar('\n');
+		j++;
 	}
-	_putchar('\n');
 }
