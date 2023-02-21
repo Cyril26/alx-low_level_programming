a555fd328608b32582f6161cc83258dd340c25af
Cyril26 <cyrilodm26@gmail.com>
2023-02-21 12:56:59 -0800

    add 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Return: ALways 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int letters[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int size, cnt;
+
+	size = sizeof(letters)/sizeof(int);
+	for (cnt = 0; cnt < size; cnt++)
+	{
+		_putchar(letters[cnt]);
+	}
+	_putchar('\n');
+	return (0);
+
+}
