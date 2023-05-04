693178231cf452f203e4a7c5ae419af953b16805
Cyril26 <cyrilodm26@gmail.com>
2023-05-04 14:49:41 -0700

    binary to unit file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,37 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary num to
+ * binary_to_uint - converts a binary number to an
  * unsigned int.
- * @n: binary.
+ * @b: binary.
  *
  * Return: unsigned int.
  */
-unsigned int binary_to_uint(const char *n)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int a;
-	int length, b_two;
+	unsigned int ui;
+	int len, base_two;
 
-	if (!n)
+	if (!b)
 		return (0);
 
-	a = 0;
+	ui = 0;
 
-	for (length = 0; b[length] != '\0'; length++)
+	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (length--, b_two = 1; length >= 0; length--, b_two *= 2)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (n[length] != '0' && n[length] != '1')
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
 
-		if (n[length] & 1)
+		if (b[len] & 1)
 		{
-			a += b_two;
+			ui += base_two;
 		}
 	}
 
-	return (a);
+	return (ui);
 }
